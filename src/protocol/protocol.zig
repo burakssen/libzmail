242b0d2a4fc81558f6dc44959e38a72f29ada8ab
Burak Şen <buraksen7@hotmail.com>
2026-01-31 09:48:08 +0100

    Fix

--- src/protocol/protocol.zig
+++ src/protocol/protocol.zig
@@ -1,8 +1,7 @@
 pub const SmtpProtocol = @import("smtp.zig").SmtpProtocol;
 pub const ImapProtocol = @import("imap.zig").ImapProtocol;
 
-pub const common = @import("common.zig");
-pub const MailPayload = common.MailPayload;
+pub const MailPayload = @import("smtp/types.zig").MailPayload;
 
 pub const imap = @import("imap.zig");
 pub const smtp = @import("smtp.zig");
