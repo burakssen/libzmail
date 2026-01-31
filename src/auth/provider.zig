88ec4a0e252f38e0aa3049cbccbeaff064c379ca
Burak Şen <buraksen7@hotmail.com>
2026-01-31 08:58:36 +0100

    Make it more concise

--- src/auth/provider.zig
+++ src/auth/provider.zig
@@ -13,25 +13,26 @@ pub fn Provider(comptime tag: std.meta.Tag(types.PayloadType)) type {
     };
 
     return struct {
+        const Self = @This();
         pub const PayloadTag = tag;
 
         allocator: std.mem.Allocator,
         impl: ?Impl = null,
 
-        pub fn init(allocator: std.mem.Allocator, payload: Payload) @This() {
+        pub fn init(allocator: std.mem.Allocator, payload: Payload) Self {
             return .{
                 .allocator = allocator,
                 .impl = Impl.init(allocator, payload),
             };
         }
 
-        pub fn deinit(self: *@This()) void {
+        pub fn deinit(self: *Self) void {
             if (self.impl) |*impl| {
                 impl.deinit();
             }
         }
 
-        pub fn authenticate(self: *@This(), curl: *c.CURL) !void {
+        pub fn authenticate(self: *Self, curl: *c.CURL) !void {
             if (self.impl) |*impl| {
                 try impl.authenticate(curl);
             }
